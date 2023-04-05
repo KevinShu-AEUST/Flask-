0ff74779edfe438485b36de65dc9bce904d2c3ab
Kevin <74130431+KevinShu-AEUST@users.noreply.github.com>
2023-04-05 09:24:37 +0800

    最新

--- training/app.py
+++ training/app.py
@@ -1,5 +1,5 @@
-
-from flask import Flask#載入Flask
+from flask import Flask #載入Flask
+from flask import request #載入 Request物件
 # coding=UTF-8
 
 #建立Appkication 物件
@@ -11,9 +11,28 @@ app=Flask(
 #所有在 public 資料夾底下的檔案，都對應到網址路徑 /www/檔案名稱
 
 #建立路徑 / 對應的處理函式
-@app.route("/")#函式的裝飾(Deractor):以函式為基礎，提供附加的功能
-def home():
-    return 'Hello Flask '
+@app.route('/')
+def index():
+    # print('請求方法',request.method)
+    # print('通訊協定',request.scheme)
+    # print('主機名稱',request.host)
+    # print('路徑',request.path)
+    # print('完整的網址',request.url)
+    # print('瀏覽器和作業系統',request.headers.get('user-agent'))
+    # print('語言偏好',request.headers.get('accept-language'))
+    # print('引薦網址',request.headers.get('referrer'))
+    lang=request.headers.get('accept-language')
+    print('語言偏好',lang)
+    if lang.startswith('en'):
+        return 'Hello Flask '
+    else:
+        return '您好，歡迎光臨'
+    
+        
+    
+
+
+    
 @app.route('/test')#代表我們要處理的網站路徑
 def test():
     return 'This is Test'
